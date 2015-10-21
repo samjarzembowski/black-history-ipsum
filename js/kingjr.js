37d02facfdd76954dbe4222c670ad160bb98d061
samjarzembowski <sjarzembowski@gmail.com>
2015-10-21 15:38:14 -0700

    Ipsum works for one author and words, but not paragraphs.  in process of movingtext blocks to individual js files

---
+++ js/kingjr.js
@@ -0,0 +1,9 @@
+var kingjr = new object(),
+  name:
+  shortname:
+  text:
+  text1:
+  text2:
+  text3:
+  text4:
+  text5:
---
+++ js/obama.js
@@ -0,0 +1,9 @@
+var obama = new object(),
+  name:
+  shortname:
+  text:
+  text1:
+  text2:
+  text3:
+  text4:
+  text5:
